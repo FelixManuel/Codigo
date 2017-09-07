d645eb81570c75780fda63ae7bbc1db09261a9c0
Felix Manuel <felixmanuel88@gmail.com>
2017-09-07 23:15:15 +0200

    More listo operations
    I have done this task:
    - Include more list rotate operations.

--- Java/Operadores/src/List/List.java
+++ Java/Operadores/src/List/List.java
@@ -19,4 +19,46 @@ public class List {
         }
         return vector;
     }
+    
+    public int[] rotateBetweenPositions(int[] vector, int start, int end){
+        int number = vector[start];
+        return rotateBetweenPositions(vector, start, end, number);
+    }
+    
+    private int[] rotateBetweenPositions(int[] vector, int start, int end, int number){
+        if(start == end){
+            vector[start] = number;
+        }else{
+            vector[start] = vector[start+1];
+            rotateBetweenPositions(vector, start+1, end, number);
+        }
+        return vector;
+    }
+    
+    public int[] rotateKElements(int[] vector, int elements){
+        int number = vector[0];
+        return rotateKElements(vector,0,vector.length-1,elements,number);
+    }
+    
+    private int[] rotateKElements(int[] vector, int start, int end, int elements, int number){
+        if(elements != 0){
+            number = vector[start];
+            vector = rotate(vector);
+            vector = rotateKElements(vector, start, end, elements-1, number);
+        }
+        return vector;
+    }
+    
+    public int josefo(int[] vector, int rotation){
+        return josefo(vector,rotation,vector.length);
+    }
+    
+    private int josefo(int[] vector, int rotation, int vectorLength){
+        if(vectorLength == 1){
+            return vector[0];
+        }else{
+            vector = rotateKElements(vector, 0, vectorLength, rotation, vector[0]);
+            return josefo(vector, rotation, vectorLength-1);
+        }
+    }
 }
--- Java/Operadores/src/Main/Main.java
+++ Java/Operadores/src/Main/Main.java
@@ -12,11 +12,9 @@ public class Main {
         Operators operators = new Operators();
         List list = new List();
         
-        int[] vector = {5,7,2,2,6,9,77,5,3,22,23};
-        int[] result = list.rotate(vector);
+        int[] vector = {5,7,2,2,6,9,77,5};
+        int result = list.josefo(vector, 2);
         
-        for(int number:result){
-            System.out.print(number + " ");
-        }
+        System.out.println(result);
     }
 }
